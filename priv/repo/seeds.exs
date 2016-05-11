6f134fdca2351448d55371cad46626a83c11ba2a
Alberto Rodriguez <arod.unerg@gmail.com>
2016-05-10 20:26:12 -0400

    build recover_tree

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -6,27 +6,26 @@ alias Test.Repo
 
 Repo.delete_all(Category)
 
-#categories = [
-#  %Category{id: 1, name: "uno", parent_id: 0, lft: 1, rgt: 12},
-#  %Category{id: 2, name: "dos", parent_id: 1, lft: 2, rgt: 5},
-#  %Category{id: 3, name: "tres", parent_id: 2, lft: 3, rgt: 4},
-#  %Category{id: 4, name: "cuatro", parent_id: 1, lft: 6, rgt: 11},
-#  %Category{id: 5, name: "cinco", parent_id: 4, lft: 7, rgt: 8},
-#  %Category{id: 6, name: "seis", parent_id: 4, lft: 9, rgt: 10}
-#]
-
 categories = [
-  %Category{id: 1, name: "uno", parent_id: 0, lft: nil, rgt: nil},
-  %Category{id: 2, name: "dos", parent_id: 1, lft: nil, rgt: nil},
-  %Category{id: 3, name: "tres", parent_id: 2, lft: nil, rgt: nil},
-  %Category{id: 4, name: "cuatro", parent_id: 1, lft: nil, rgt: nil},
-  %Category{id: 5, name: "cinco", parent_id: 4, lft: nil, rgt: nil},
-  %Category{id: 6, name: "seis", parent_id: 4, lft: nil, rgt: nil}
+  %Category{id: 1, name: "uno", parent_id: 0},
+    %Category{id: 2, name: "dos", parent_id: 1},
+      %Category{id: 3, name: "tres", parent_id: 2},
+  %Category{id: 4, name: "cuatro", parent_id: 0},
+    %Category{id: 5, name: "cinco", parent_id: 4},
+      %Category{id: 6, name: "seis", parent_id: 5},
+        %Category{id: 7, name: "siete", parent_id: 6},
+          %Category{id: 8, name: "ocho", parent_id: 7},
+      %Category{id: 9, name: "nueve", parent_id: 5},
+      %Category{id: 10, name: "diez", parent_id: 5},
+    %Category{id: 11, name: "once", parent_id: 4},
+      %Category{id: 12, name: "doce", parent_id: 11}
 ]
 
 Enum.map(categories, fn category ->
   Repo.insert!(category)
 end)
+
+Category.recover_tree(0,0)
 
 #1. uno (1,12)
 #  2. dos (2,5)
